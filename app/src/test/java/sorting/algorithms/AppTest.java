4b7a9e1ca1a52528b8f3f9cd73b9a9e66fd26db6
rina-reimer <rinareimer616@gmail.com>
2024-02-12 10:43:16 -0800

    updates to ocaml implementation

--- app/src/test/java/sorting/algorithms/AppTest.java
+++ app/src/test/java/sorting/algorithms/AppTest.java
@@ -1,4 +1,4 @@
-package sorting;
+package sorting.algorithms;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
---
+++ ocaml/sort.mli
@@ -0,0 +1,4 @@
+val is_sorted : 'a list -> bool
+val insert : 'a * 'a list -> 'a list
+val isort : 'a list -> 'a list
+val qsort : ('a * 'a -> bool) * 'a list -> 'a list
